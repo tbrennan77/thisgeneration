fe376f361585e11b0535c4728bbede0a127f1b96
Timothy Brennan <2timothy215@gmail.com>
2021-03-10 15:18:47 -0500

    Add Gulp file

---
+++ wp-content/themes/thisgeneration/gulp.js
@@ -0,0 +1,89 @@
+// Gulp.js configuration
+'use strict';
+
+const
+
+  // source and build folders
+  dir = {
+    src         : 'src/',
+    build       : '/Users/tbrennan/websites/cmi/wp-content/themes/thisgeneration/'
+  },
+
+  // Gulp and plugins
+  gulp          = require('gulp'),
+  gutil         = require('gulp-util'),
+  newer         = require('gulp-newer'),
+  imagemin      = require('gulp-imagemin'),
+  sass          = require('gulp-sass'),
+  postcss       = require('gulp-postcss'),
+  deporder      = require('gulp-deporder'),
+  concat        = require('gulp-concat'),
+  stripdebug    = require('gulp-strip-debug'),
+  uglify        = require('gulp-uglify')
+;
+
+// Browser-sync
+var browsersync = false;
+
+
+// PHP settings
+const php = {
+  src           : dir.src + 'template/**/*.php',
+  build         : dir.build
+};
+
+// copy PHP files
+gulp.task('php', () => {
+  return gulp.src(php.src)
+    .pipe(newer(php.build))
+    .pipe(gulp.dest(php.build));
+});
+
+// image settings
+const images = {
+  src         : dir.src + 'images/**/*',
+  build       : dir.build + 'images/'
+};
+
+// image processing
+gulp.task('images', () => {
+  return gulp.src(images.src)
+    .pipe(newer(images.build))
+    .pipe(imagemin())
+    .pipe(gulp.dest(images.build));
+});
+
+
+// CSS settings
+var css = {
+  src         : dir.src + 'scss/style.scss',
+  watch       : dir.src + 'scss/**/*',
+  build       : dir.build,
+  sassOpts: {
+    outputStyle     : 'nested',
+    imagePath       : images.build,
+    precision       : 3,
+    errLogToConsole : true
+  },
+  processors: [
+    require('postcss-assets')({
+      loadPaths: ['images/'],
+      basePath: dir.build,
+      baseUrl: '/wp-content/themes/thisgeneration/'
+    }),
+    require('autoprefixer')({
+      browsers: ['last 2 versions', '> 2%']
+    }),
+    require('css-mqpacker'),
+    require('cssnano')
+  ]
+};
+
+// CSS processing
+gulp.task('css', ['images'], () => {
+  return gulp.src(css.src)
+    .pipe(sass(css.sassOpts))
+    .pipe(postcss(css.processors))
+    .pipe(gulp.dest(css.build))
+    .pipe(browsersync ? browsersync.reload({ stream: true }) : gutil.noop());
+});
