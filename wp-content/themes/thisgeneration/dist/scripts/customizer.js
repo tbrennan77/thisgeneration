ec940b1978adba3a4447231181acb3249645d75c
Timothy Brennan <2timothy215@gmail.com>
2021-03-10 12:58:10 -0500

    yarn build run 1st time

--- wp-content/themes/thisgeneration/dist/scripts/customizer.js
+++ wp-content/themes/thisgeneration/dist/scripts/customizer.js
@@ -61,7 +61,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "be3272d07fd8fccc9736"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "3497082e0cb6d58fb34e"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -716,7 +716,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "http://localhost:3000/dist/";
+/******/ 	__webpack_require__.p = "http://localhost:3000/wp-content/themes/thisgeneration/dist/";
 /******/
 /******/ 	// __webpack_hash__
 /******/ 	__webpack_require__.h = function() { return hotCurrentHash; };
