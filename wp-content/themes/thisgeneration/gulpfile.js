57aaf2a132fb9cf25029fc16e3d6fb5d93d821f6
Timothy Brennan <2timothy215@gmail.com>
2021-03-11 14:20:25 -0500

    Update Gulp to version 4

--- wp-content/themes/thisgeneration/gulpfile.js
+++ wp-content/themes/thisgeneration/gulpfile.js
@@ -1,89 +1,159 @@
-// Gulp.js configuration
-'use strict';
+// Load all the modules from package.json
+var gulp = require( 'gulp' ),
+  plumber = require( 'gulp-plumber' ),
+  autoprefixer = require('gulp-autoprefixer'),
+  watch = require( 'gulp-watch' ),
+  jshint = require( 'gulp-jshint' ),
+  stylish = require( 'jshint-stylish' ),
+  uglify = require( 'gulp-uglify' ),
+  rename = require( 'gulp-rename' ),
+  notify = require( 'gulp-notify' ),
+  include = require( 'gulp-include' ),
+  sass = require( 'gulp-sass' ),
+  browserSync = require('browser-sync').create(),
+  critical = require('critical'),
+  zip = require('gulp-zip');
 
-const
+var config = {
+     nodeDir: './node_modules' 
+}
 
-  // source and build folders
-  dir = {
-    src         : 'src/',
-    build       : '/Users/tbrennan/websites/cmi/wp-content/themes/thisgeneration/'
-  },
+// Default error handler
+var onError = function( err ) {
+  console.log( 'An error occured:', err.message );
+  this.emit('end');
+}
 
-  // Gulp and plugins
-  gulp          = require('gulp'),
-  gutil         = require('gulp-util'),
-  newer         = require('gulp-newer'),
-  imagemin      = require('gulp-imagemin'),
-  sass          = require('gulp-sass'),
-  postcss       = require('gulp-postcss'),
-  deporder      = require('gulp-deporder'),
-  concat        = require('gulp-concat'),
-  stripdebug    = require('gulp-strip-debug'),
-  uglify        = require('gulp-uglify')
-;
+// JS to watch
+var jsFiles = [
+  './js/**/*.js', 
+  '!./js/dist/*.js',
+];
+ 
+// Sass files to watch
+var cssFiles = [
+  './src/scss/**/*.scss'
+];
 
-// Browser-sync
-var browsersync = false;
+// automatically reloads the page when files changed
+var browserSyncWatchFiles = [
+    './*.min.css',
+    './js/**/*.min.js',
+    './**/*.php'
+];
 
+// see: https://www.browsersync.io/docs/options/
+var browserSyncOptions = {
+    watchTask: true,
+    proxy: "http://localhost:8888/"
+}
 
-// PHP settings
-const php = {
-  src           : dir.src + 'template/**/*.php',
-  build         : dir.build
-};
-
-// copy PHP files
-gulp.task('php', () => {
-  return gulp.src(php.src)
-    .pipe(newer(php.build))
-    .pipe(gulp.dest(php.build));
+// Zip files up
+gulp.task('zip', function () {
+ return gulp.src([
+   '*',
+   './css/*',
+   './fonts/*',
+   './images/**/*',
+   './inc/**/*',
+   './js/**/*',
+   './languages/*',
+   './scss/**/*',
+   './template-parts/*',
+   './templates/*',
+   '!bower_components',
+   '!node_modules',
+  ], {base: "."})
+  .pipe(zip('strappress.zip'))
+  .pipe(gulp.dest('.'));
+});
+ 
+// Jshint outputs any kind of javascript problems you might have
+// Only checks javascript files inside /src directory
+gulp.task( 'jshint', function() {
+  return gulp.src( './js/src/*.js' )
+    .pipe( jshint() )
+    .pipe( jshint.reporter( stylish ) )
+    .pipe( jshint.reporter( 'fail' ) );
+})
+ 
+ 
+// Concatenates all files that it finds in the manifest
+// and creates two versions: normal and minified.
+// It's dependent on the jshint task to succeed.
+gulp.task( 'scripts', gulp.series('jshint'), function() {
+  return gulp.src( './js/manifest.js' )
+    .pipe( include() )
+    .pipe( rename( { basename: 'scripts' } ) )
+    .pipe( gulp.dest( './js/dist' ) )
+    // Normal done, time to create the minified javascript (scripts.min.js)
+    // remove the following 3 lines if you don't want it
+    .pipe( uglify() )
+    .pipe( rename( { suffix: '.min' } ) )
+    .pipe( gulp.dest( './js/dist' ) )
+    .pipe(browserSync.reload({stream: true}))
+    .pipe( notify({ message: 'scripts task complete' }));
 });
 
-// image settings
-const images = {
-  src         : dir.src + 'images/**/*',
-  build       : dir.build + 'images/'
-};
+// Sass - Creates a regular and minified .css file in root 
+gulp.task('sass', function() {
+  return gulp.src('./src/scss/style.scss')
+    .pipe(plumber())
+    .pipe(sass({
+        errLogToConsole: true,
+        precision: 8,
+        noCache: true,
+        //imagePath: 'assets/img',
+        includePaths: [
+          config.nodeDir + '/bootstrap/scss',
+        ]
+      }).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('.'))
+    .pipe(sass({ outputStyle:'compressed'}).on('error', sass.logError))
+    .pipe(rename( { suffix: '.min' } ) )
+    .pipe(gulp.dest('.'))
+    .pipe(browserSync.reload({stream: true}))
+    .pipe(notify({ title: 'Sass', message: 'sass task complete'  }));
+});
 
-// image processing
-gulp.task('images', () => {
-  return gulp.src(images.src)
-    .pipe(newer(images.build))
-    .pipe(imagemin())
-    .pipe(gulp.dest(images.build));
+// Generate & Inline Critical-path CSS
+gulp.task('critical', function (cb) {
+    critical.generate({
+        base: './',
+        src: 'http://localhost:8888/',
+        dest: 'css/home.min.css',
+        ignore: ['@font-face'],
+        dimensions: [{
+          width: 320,
+          height: 480
+        },{
+          width: 768,
+          height: 1024
+        },{
+          width: 1280,
+          height: 960
+        }],
+        minify: true
+    });
 });
 
 
-// CSS settings
-var css = {
-  src         : dir.src + 'scss/style.scss',
-  watch       : dir.src + 'scss/**/*',
-  build       : dir.build,
-  sassOpts: {
-    outputStyle     : 'nested',
-    imagePath       : images.build,
-    precision       : 3,
-    errLogToConsole : true
-  },
-  processors: [
-    require('postcss-assets')({
-      loadPaths: ['images/'],
-      basePath: dir.build,
-      baseUrl: '/wp-content/themes/thisgeneration/'
-    }),
-    require('autoprefixer')({
-      browsers: ['last 2 versions', '> 2%']
-    }),
-    require('css-mqpacker'),
-    require('cssnano')
-  ]
-};
-
-// CSS processing
-gulp.task('css', ['images'], () => {
-  return gulp.src(css.src)
-    .pipe(sass(css.sassOpts))
-    .pipe(postcss(css.processors))
-    .pipe(gulp.dest(css.build))
-    .pipe(browsersync ? browsersync.reload({ stream: true }) : gutil.noop());
+// Starts browser-sync task for starting the server.
+gulp.task('browser-sync', function() {
+    browserSync.init(browserSyncWatchFiles, browserSyncOptions);
 });
+ 
+ 
+// Start the livereload server and watch files for change
+gulp.task( 'watch', function() {
+ 
+  // don't listen to whole js folder, it'll create an infinite loop
+  gulp.watch( jsFiles, gulp.parallel('scripts') )
+ 
+  gulp.watch( cssFiles, gulp.parallel('sass') );
+   
+} );
+ 
+ 
+gulp.task( 'default', gulp.parallel('watch', 'browser-sync'));
